a276313790b18ae6931be27e7a055833f80bbc04
slamensd <58567346+slamensd@users.noreply.github.com>
2023-05-24 03:12:25 +0000

    go

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-const provider = new ethers.providers.Web3Provider(window.ethereum);
+onst provider = new ethers.providers.Web3Provider(window.ethereum);
 let signer;
+let nftTokenContract;
+let stakingContract;
 
 const nftTokenAddress = '0xdf9669a65c5845e472ad3ca83d07605a9d7701b7';
 const treasuryTokenAddress = '0x232765be70a5f0b49e2d72eee9765813894c1fc4';
 const stakingContractAddress = 'YOUR_STAKING_CONTRACT_ADDRESS';  // replace with your contract's address
 const treasuryTokenId = 'YOUR_TREASURY_TOKEN_ID';  // replace with your token's ID
 
-const nftTokenContract = new ethers.Contract(nftTokenAddress, ['function balanceOf(address owner) view returns (uint256)', 'function tokenOfOwnerByIndex(address owner, uint256 index) view returns (uint256)'], provider);
-const stakingContract = new ethers.Contract(stakingContractAddress, ['function stakeNFT(uint256[] calldata tokenIds) external', 'function unstake() external'], signer);
-
 // Connect wallet button
 document.getElementById('connectButton').addEventListener('click', async () => {
     await window.ethereum.enable();
     signer = provider.getSigner();
+    nftTokenContract = new ethers.Contract(nftTokenAddress, ['function balanceOf(address owner) view returns (uint256)', 'function tokenOfOwnerByIndex(address owner, uint256 index) view returns (uint256)'], provider);
+    stakingContract = new ethers.Contract(stakingContractAddress, ['function stakeNFT(uint256[] calldata tokenIds) external', 'function unstake() external'], signer);
     document.getElementById('stakeSection').classList.remove('hidden');
     await loadTokens();
 });

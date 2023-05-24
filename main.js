0f55ca1511492525ac2c8cf4395b3223098bf887
slamensd <58567346+slamensd@users.noreply.github.com>
2023-05-24 03:15:27 +0000

    fff

--- main.js
+++ main.js
@@ -10,49 +10,56 @@ const treasuryTokenId = 'YOUR_TREASURY_TOKEN_ID';  // replace with your token's 
 
 // Connect wallet button
 document.getElementById('connectButton').addEventListener('click', async () => {
-    await window.ethereum.enable();
-    signer = provider.getSigner();
-    nftTokenContract = new ethers.Contract(nftTokenAddress, ['function balanceOf(address owner) view returns (uint256)', 'function tokenOfOwnerByIndex(address owner, uint256 index) view returns (uint256)'], provider);
-    stakingContract = new ethers.Contract(stakingContractAddress, ['function stakeNFT(uint256[] calldata tokenIds) external', 'function unstake() external'], signer);
-    document.getElementById('stakeSection').classList.remove('hidden');
-    await loadTokens();
+    try {
+        await window.ethereum.enable();
+        signer = provider.getSigner();
+        nftTokenContract = new ethers.Contract(nftTokenAddress, ['function balanceOf(address owner) view returns (uint256)', 'function tokenOfOwnerByIndex(address owner, uint256 index) view returns (uint256)'], provider);
+        stakingContract = new ethers.Contract(stakingContractAddress, ['function stakeNFT(uint256[] calldata tokenIds) external', 'function unstake() external'], signer);
+        document.getElementById('stakeSection').classList.remove('hidden');
+        await loadTokens();
+    } catch (error) {
+        console.error('Error in wallet connection: ', error);
+    }
 });
 
 async function loadTokens() {
-    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-    const balance = await nftTokenContract.balanceOf(accounts[0]);
-    const tokenListDiv = document.getElementById('tokenList');
-    tokenListDiv.innerHTML = '';
+    try {
+        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+        const balance = await nftTokenContract.balanceOf(accounts[0]);
+        const tokenListDiv = document.getElementById('tokenList');
+        tokenListDiv.innerHTML = '';
 
-    for(let i = 0; i < balance; i++) {
-        const tokenId = await nftTokenContract.tokenOfOwnerByIndex(accounts[0], i);
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.value = tokenId.toString();
-        tokenListDiv.appendChild(checkbox);
-        tokenListDiv.appendChild(document.createTextNode(` Token ID: ${tokenId.toString()}`));
-        tokenListDiv.appendChild(document.createElement('br'));
+        for(let i = 0; i < balance; i++) {
+            const tokenId = await nftTokenContract.tokenOfOwnerByIndex(accounts[0], i);
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.value = tokenId.toString();
+            tokenListDiv.appendChild(checkbox);
+            tokenListDiv.appendChild(document.createTextNode(` Token ID: ${tokenId.toString()}`));
+            tokenListDiv.appendChild(document.createElement('br'));
+        }
+    } catch (error) {
+        console.error('Error in token loading: ', error);
     }
 }
 
 // Stake NFTs button
 document.getElementById('stakeButton').addEventListener('click', async () => {
-    const checkboxes = Array.from(document.getElementById('tokenList').querySelectorAll('input[type=checkbox]'));
-    const selectedTokenIds = checkboxes.filter(cb => cb.checked).map(cb => ethers.BigNumber.from(cb.value));
-
-    if (selectedTokenIds.length !== 5) {
-        alert('You must select exactly 5 NFTs to stake.');
-        return;
-    }
-
     try {
+        const checkboxes = Array.from(document.getElementById('tokenList').querySelectorAll('input[type=checkbox]'));
+        const selectedTokenIds = checkboxes.filter(cb => cb.checked).map(cb => ethers.BigNumber.from(cb.value));
+
+        if (selectedTokenIds.length !== 5) {
+            alert('You must select exactly 5 NFTs to stake.');
+            return;
+        }
+
         const tx = await stakingContract.connect(signer).stakeNFT(selectedTokenIds);
         await tx.wait();
         alert('NFTs staked successfully!');
         await loadTokens();
     } catch (error) {
-        console.error('An error occurred', error);
-        alert('An error occurred. Please see the console for more details.');
+        console.error('Error in staking NFTs: ', error);
     }
 });
 
@@ -64,7 +71,6 @@ document.getElementById('unstakeButton').addEventListener('click', async () => {
         alert('Treasury Token unstaked successfully!');
         await loadTokens();
     } catch (error) {
-        console.error('An error occurred', error);
-        alert('An error occurred. Please see the console for more details.');
+        console.error('Error in unstaking: ', error);
     }
 });
